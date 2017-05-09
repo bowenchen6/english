1704f1404e35376ecf99a51789241d9f75bd565b
BowenChen <bowenchen886@gmail.com>
2017-05-09 15:27:49 +0800

    add mongodb support

--- server.go
+++ server.go
@@ -7,7 +7,13 @@ import (
 	"os"
 
 	"github.com/urfave/negroni"
+	"gopkg.in/mgo.v2"
+	"gopkg.in/mgo.v2/bson"
 )
+
+type Word struct {
+	Name string
+}
 
 func main() {
 	port := os.Getenv("PORT")
@@ -18,7 +24,30 @@ func main() {
 
 	mux := http.NewServeMux()
 	mux.HandleFunc("/", func(w http.ResponseWriter, req *http.Request) {
-		fmt.Fprintf(w, "Hello World!")
+		mongoURL := os.Getenv("MONGODB_URI")
+		if mongoURL == "" {
+			log.Fatal("$MONGODB_URI must be set")
+		}
+
+		session, err := mgo.Dial(mongoURL)
+		if err != nil {
+			log.Fatal(err)
+		}
+		defer session.Close()
+
+		c := session.DB("heroku_n1ctsmn2").C("word")
+		err = c.Insert(&Word{"negligence",}, &Word{"vulnerabilities",}, &Word{"drawback",}, &Word{"misuse",}, &Word{"damage",})
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		result := Word{}
+		err = c.Find(bson.M{"name":"negligence"}).One(&result)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		fmt.Fprintf(w, result.Name)
 	})
 
 	n := negroni.Classic() // Includes some default middlewares
